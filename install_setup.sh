b98ba3260003cc706a482467bbd0643052c27081
8408323 <j.haraldsson94@gmail.com>
2020-04-15 10:29:50 +0200

    Updated installation script with fiji/ImageJ, vlc and libre office. Also made some minor esthetical changes to the code.

--- install_setup.sh
+++ install_setup.sh
@@ -8,18 +8,31 @@ program_exists () {
 ### Script ###
 cd "$(dirname "$0")"
 
-sudo apt-get update >/dev/null
-sudo apt-get -y install neovim thunderbird screen zsh --fix-missing
+echo -e "\n" | sudo add-apt-repository ppa:teejee2008/ppa
+
+sudo apt-get update
+sudo apt-get -y install neovim thunderbird screen nmap net-tools zsh openssh-server --fix-missing
+sudo apt-get -y upgrade
+sudo apt-get -y autoremove
+sudo snap install slack --classic
+sudo snap install teams-for-linux
+sudo snap install vlc
+sudo snap install libreoffice
+
+# Setup SSH to this computer
+sudo ufw allow ssh
+
+# TODO(joha): Define which sections below to install, make them optional. Maybe programs above as well.
 
 # Install vpn --- https://uci.service-now.com/kb_view.do?sysparm_article=KB0010201
 if ! program_exists vpn
 then
     sudo apt-get -y install libpangox-1.0-0 libgtk2.0-0:i386
-    tar -xvzf vpn/anyconnect-linux64-4.6.02074-predeploy-k9.tar.gz -C vpn/ >/dev/null 2>&1
+    tar -xvzf vpn/anyconnect-linux64-4.6.02074-predeploy-k9.tar.gz -C vpn/ > /dev/null 2>&1
     cd vpn/anyconnect-linux64-4.6.02074/vpn
     echo -e "y\n" | sudo ./vpn_install.sh
     echo "export PATH=$PATH:/opt/cisco/anyconnect/bin/" >> ~/.bashrc
-    cd - >/dev/null 2>&1
+    cd - > /dev/null 2>&1
 fi
 
 # Install chrome
@@ -31,12 +44,24 @@ then
 fi
 
 # ZSH
-echo $(pwd)
 if ! program_exists zsh
 then
-    cp zsh/.* ~/ >/dev/null 2>&1
+    cp zsh/.* ~/ > /dev/null 2>&1
     chsh -s $(which zsh)
 fi
+
+# FIJI/ImageJ
+if ! program_exists fiji
+then
+    wget -P ~/Downloads/ https://downloads.imagej.net/fiji/latest/fiji-linux64.zip
+    mkdir -p ~/programs
+    unzip ~/Downloads/fiji-linux64.zip -d ~/programs/ > /dev/null 2>&1
+    sudo rm /usr/bin/fiji > /dev/null 2>&1
+    sudo ln -s ~/programs/Fiji.app/ImageJ-linux64 /usr/bin/fiji
+    #sudo chmod +rwx ~/programs/Fiji.app/ImageJ-linux64
+    rm ~/Downloads/fiji-linux64.zip
+fi
+
 
 # Install i3
 
